e3605eba94dea0f1612f76635d5b0ff17f5d47f1
Adam Hanna <ahanna@alumni.mines.edu>
2021-08-13 13:25:45 -0700

    minor changes

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from './watcher'
+export { default as Watcher } from './watcher'
+
+export * from './throttle'
+export { default as Throttle } from './throttle'
+
+export * from './pubsub'
+export { default as Pubsub } from './pubsub'
